d580dc87b45565341542f5d8ae525befe101d18e
peter varga <pkvarg@yahoo.se>
2023-09-26 13:04:35 +0200

    jezis half

--- src/pages/Jezis.jsx
+++ src/pages/Jezis.jsx
@@ -1,7 +1,211 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Jezis = () => {
-  return <div>Jezis</div>
+  return (
+    <>
+      <Header />
+      <div className='bg-white text-[20px]'>
+        <h1 className='text-center text-[30px] py-8'>Je Ježiš Boh?</h1>
+        <div className='py-8 mx-4 lg:mx-8'>
+          <h2 className='text-center text-[25px]'>Je Ježiš Boh?</h2>
+          <p>
+            Byť Bohom znamená mať jedinečné božské vlastnosti, ktoré nemá žiadna
+            stvorená bytosť. Božie Slovo vymedzuje rôzne oblasti, ktoré
+            prislúchajú len Bohu a ktoré má človek zakázané prisudzovať
+            ktorejkoľvek stvorenej bytosti.
+          </p>
+          <h2 className='text-center text-[25px]'>Len Boh môže stvoriť </h2>
+          <p>
+            <span className='italic'> Na počiatku stvoril Boh nebo a zem.</span>
+            <span className='text-[20px]'> Genesis 1:1 </span>
+            <span className='italic'>
+              {' '}
+              Ním (Slovom, ktoré sa stalo telom) povstalo všetko a bez Neho
+              nepovstalo nič, čo povstalo.{' '}
+            </span>
+            <span className='text-[20px]'> Ján 1:3 </span>
+            <span className='italic'>
+              {' '}
+              Bolo vo svete a svet Ním povstal, ale svet Ho nepoznal. Do svojho
+              vlastného prišiel, a Jeho vlastní Ho neprijali.{' '}
+            </span>
+            <span className='text-[20px]'> Ján 1:10-11 </span>
+            Svet Ježišom povstal, bol Ním stvorený.
+          </p>
+          <h2 className='text-center text-[25px] mt-4'>Len Boh môže súdiť</h2>
+          <p>
+            <span className='italic'>
+              {' '}
+              A Otec ani nesúdi nikoho, ale celý súd odovzdal Synovi, aby všetci
+              ctili Syna tak, ako ctia Otca.
+            </span>
+            <span className='text-[20px]'> Ján 5:22 </span>
+            Boh zveril Ježišovi Svoju autoritu.
+          </p>
+
+          <h2 className='text-center text-[25px] mt-4'>
+            Len Boha máme uctievať
+          </h2>
+
+          <p>
+            <span className='italic'>
+              {' '}
+              A Otec ani nesúdi nikoho, ale celý súd odovzdal Synovi, aby všetci
+              ctili Syna tak, ako ctia Otca. Kto nectí Syna, nectí ani Otca,
+              ktorý Ho poslal.
+            </span>
+            <span className='text-[20px]'> Ján 5:22-23 </span>
+            Máme vzdať česť a slávu Ježišovi, inak si nectíme Boha.
+          </p>
+
+          {/* continue */}
+
+          <h2 className='text-center text-[25px] mt-4'>
+            Svedčili o kráľovstve Božom, do ktorého vstúpi človek len skrze
+            vieru v Ježiša Krista
+          </h2>
+          <p>
+            <span className='italic'>
+              {' '}
+              A celkom smelo a bez prekážky kázal o kráľovstve Božom a učil o
+              Pánovi Ježišovi Kristovi.{' '}
+            </span>
+            <span className='text-[20px]'> Skutky apoštolov 28:31 </span>
+            <span className='italic'>
+              {' '}
+              Amen, amen, hovorím ti: Ak sa niekto nenarodí z vody a z Ducha,
+              nemôže vojsť do Božieho kráľovstva. Na to mu Nikodém odpovedal:
+              „Ako sa to môže stať?“ A ako Mojžiš vyzdvihol hada na púšti, tak
+              musí byť vyzdvihnutý aj Syn človeka, aby každý, kto v neho verí,
+              mal večný život.{' '}
+            </span>
+            <span className='text-[20px]'> Ján 3:5,9,14-15 </span>
+            Kázanie o kráľovstve je opäť len kázanie o Ježišovi Kristovi.
+          </p>
+
+          <h2 className='text-center text-[25px] mt-4'>
+            Svedčili o osobe Ježiša Krista
+          </h2>
+
+          <p>
+            <span className='italic'>
+              {' '}
+              S mužmi, ktorí životy nasadili pre meno nášho Pána Ježiša Krista.{' '}
+            </span>
+            <span className='text-[20px]'> Skutky apoštolov 15:26 </span>
+            Tak zvestovali evanjelium, že riskovali vlastné životy za svedectvo
+            o Pánovi Ježišovi.
+            <span className='italic'>
+              {' '}
+              Ale niektorí z nich boli z Cypru a z Kyrény, a keď prišli do
+              Antiochie, hovorili aj Grékom a zvestovali Pána Ježiša.{' '}
+            </span>
+            <span className='text-[20px]'> Skutky apoštolov 11:20 </span>
+            <span className='italic'>
+              {' '}
+              Predvolali si ich teda a prikázali im, aby nikdy viac ani
+              nehovorili ani neučili v mene Ježišovom.{' '}
+            </span>
+            <span className='text-[20px]'> Skutky apoštolov 4:18 </span>
+            Čo teda vadilo ich protivníkom? Meno Ježiš.
+          </p>
+
+          <h2 className='text-center text-[25px] mt-4'>
+            Svedčili o tom, čo hovoril Ježiš Kristus
+          </h2>
+
+          <p>
+            Slovo, ktoré hlásal Pán Ježiš, to hlásali apoštoli.
+            <span className='italic'>
+              {' '}
+              O niekoľko dní povedal Pavel Barnabášovi: Vráťme sa a po všetkých
+              mestách, v ktorých sme zvestovali slovo Pánovo, ponavštevujeme
+              bratov, ako sa majú.{' '}
+            </span>
+            <span className='text-[20px]'> Skutky apoštolov 15:36 </span>
+          </p>
+
+          <h2 className='text-center text-[25px] mt-4'>
+            Ako apoštoli hlásali evanjelium?
+          </h2>
+
+          <p>
+            <span className='italic'>
+              {' '}
+              Ale prijmite moc Ducha Svätého, ktorý zostúpi na vás, a budete mi
+              svedkami.{' '}
+            </span>
+            <span className='text-[20px]'> Skutky apoštolov 1:8 </span>
+            Apoštoli nehlásali evanjelium rutinným a rozumovým rozprávaním o
+            tom, čo videli, ale v moci Ducha Svätého.
+          </p>
+
+          <h2 className='text-center text-[25px] mt-4'>
+            Čo je dôkazom pravého evanjelia?
+          </h2>
+
+          <p>
+            <span className='italic'>
+              {' '}
+              Veď ja sa nehanbím za evanjelium (Kristovo): lebo mocou Božou je
+              ono na spasenie každému veriacemu, najprv Židovi, a aj Grékovi.{' '}
+            </span>
+            <span className='text-[20px]'> Rimanom 1:16 </span>
+            Spasenie hriešnika je možné len skrze vieru v Ježiša Krista a
+            poslušnosti Jeho Slovu.
+            <span className='italic'>
+              {' '}
+              Zjavuje sa v ňom zaiste spravodlivosť Božia z viery pre vieru, ako
+              je napísané: Spravodlivý z viery bude žiť.{' '}
+            </span>
+            <span className='text-[20px]'> Rimanom 1:17 </span>
+            Vo viere vo vzkriesenie a v nasledovaní Ježiša Krista sa zjavuje
+            Božia spravodlivosť.
+          </p>
+
+          <h2 className='text-center text-[25px] mt-4'>
+            Nehlásať vzkriesenie je porušením Božieho nariadenia
+          </h2>
+
+          <p>
+            <span className='italic'>
+              {' '}
+              Veď Boh, ktorému slúžim svojím duchom zvesťou evanjelia Jeho Syna,
+              je mi svedkom, ako neprestajne spomínam na vás.{' '}
+            </span>
+            <span className='text-[20px]'> Rimanom 1:9 </span>
+            Pavol slúži Bohu tým, že zvestuje evanjelium Jeho Syna. Ak by
+            učeníci nehlásali to, čoho boli očitými svedkami, porušili by príkaz
+            Boha.
+            <span className='italic'>
+              {' '}
+              Ale Peter a Ján odpovedali im takto: Súďte, či je spravodlivé pred
+              Bohom poslúchať viac vás ako Boha! Lebo my nemôžeme nehovoriť o
+              tom, čo sme videli a počuli.{' '}
+            </span>
+            <span className='text-[20px]'> Skutky apoštolov 4:19-20 </span>
+            <span className='italic'>
+              {' '}
+              Čudujem sa, že od Toho, ktorý vás povolal v milosti Kristovej, sa
+              tak rýchlo odvraciate k inému evanjeliu, ktoré ani nie je iné
+              evanjelium. Sú však niektorí, čo robia zmätok medzi vami a chcú
+              prekrútiť evanjelium Kristovo. Ale keby sme aj my, alebo keby vám
+              anjel z neba zvestoval (iné) evanjelium miesto toho, ktoré sme vám
+              my zvestovali, - nech je prekliaty! Ako sme prv povedali, aj teraz
+              znovu hovorím: ak vám niekto zvestuje (iné) evanjelium miesto
+              toho, ktoré ste prijali, - nech je prekliaty!{' '}
+            </span>
+            <span className='text-[20px]'> Galatským 1:6-9 </span>
+            <span className='italic'> </span>
+            <span className='text-[20px]'></span>
+          </p>
+        </div>
+      </div>
+      <span className='italic'></span>
+      <span className='text-[20px]'></span>
+    </>
+  )
 }
 
 export default Jezis
