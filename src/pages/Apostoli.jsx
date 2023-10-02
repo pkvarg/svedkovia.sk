33a8774a6d7a3212ed49292c6364e9bd19334a77
peter varga <pkvarg@yahoo.se>
2023-10-02 12:28:35 +0200

    ap eng done

--- src/pages/Apostoli.jsx
+++ src/pages/Apostoli.jsx
@@ -163,154 +163,222 @@ const Apostoli = () => {
           </p>
 
           <h2 className='text-center text-[25px] mt-4'>
-            Svedčili o pokání skrze vieru v Ježiša Krista
+            {content.apSub3}
+            {/* Svedčili o pokání skrze vieru v Ježiša Krista */}
           </h2>
           <p>
             <span className='italic'>
               {' '}
-              Riekol im: Tak je napísané, že Kristus musel trpieť a na tretí deň
+              {content.apVerse11}
+              {/* Riekol im: Tak je napísané, že Kristus musel trpieť a na tretí deň
               vstať z mŕtvych a že sa musí kázať v Jeho mene pokánie na
-              odpustenie hriechov všetkým národom, počnúc od Jeruzalema.
+              odpustenie hriechov všetkým národom, počnúc od Jeruzalema. */}
             </span>
-            <span className='text-[20px]'> Lukáš 24:46-47 </span>
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef11} {/* Lukáš 24:46-47  */}
+            </span>
           </p>
 
           <h2 className='text-center text-[25px] mt-4'>
-            Svedčili o kráľovstve Božom, do ktorého vstúpi človek len skrze
-            vieru v Ježiša Krista
+            {content.apSub4}
+            {/* Svedčili o kráľovstve Božom, do ktorého vstúpi človek len skrze
+            vieru v Ježiša Krista */}
           </h2>
           <p>
             <span className='italic'>
               {' '}
-              A celkom smelo a bez prekážky kázal o kráľovstve Božom a učil o
-              Pánovi Ježišovi Kristovi.{' '}
+              {content.apVerse12}
+              {/* A celkom smelo a bez prekážky kázal o kráľovstve Božom a učil o
+              Pánovi Ježišovi Kristovi.{' '} */}
             </span>
-            <span className='text-[20px]'> Skutky apoštolov 28:31 </span>
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef12} {/* Skutky apoštolov 28:31  */}
+            </span>
             <span className='italic'>
               {' '}
-              Amen, amen, hovorím ti: Ak sa niekto nenarodí z vody a z Ducha,
+              {content.apVerse13}
+              {/* Amen, amen, hovorím ti: Ak sa niekto nenarodí z vody a z Ducha,
               nemôže vojsť do Božieho kráľovstva. Na to mu Nikodém odpovedal:
               „Ako sa to môže stať?“ A ako Mojžiš vyzdvihol hada na púšti, tak
               musí byť vyzdvihnutý aj Syn človeka, aby každý, kto v neho verí,
-              mal večný život.{' '}
+              mal večný život.{' '} */}
             </span>
-            <span className='text-[20px]'> Ján 3:5,9,14-15 </span>
-            Kázanie o kráľovstve je opäť len kázanie o Ježišovi Kristovi.
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef13} {/* Ján 3:5,9,14-15  */}
+            </span>
+            {content.apText3}
+            {/* Kázanie o kráľovstve je opäť len kázanie o Ježišovi Kristovi. */}
           </p>
 
           <h2 className='text-center text-[25px] mt-4'>
-            Svedčili o osobe Ježiša Krista
+            {content.apSub5}
+            {/* Svedčili o osobe Ježiša Krista */}
           </h2>
 
           <p>
             <span className='italic'>
               {' '}
-              S mužmi, ktorí životy nasadili pre meno nášho Pána Ježiša Krista.{' '}
+              {content.apVerse14}
+              {/* S mužmi, ktorí životy nasadili pre meno nášho Pána Ježiša Krista.{' '} */}
             </span>
-            <span className='text-[20px]'> Skutky apoštolov 15:26 </span>
-            Tak zvestovali evanjelium, že riskovali vlastné životy za svedectvo
-            o Pánovi Ježišovi.
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef14} {/* Skutky apoštolov 15:26 */}
+            </span>
+            {content.apText4}
+            {/* Tak zvestovali evanjelium, že riskovali vlastné životy za svedectvo
+            o Pánovi Ježišovi. */}
             <span className='italic'>
               {' '}
-              Ale niektorí z nich boli z Cypru a z Kyrény, a keď prišli do
-              Antiochie, hovorili aj Grékom a zvestovali Pána Ježiša.{' '}
+              {content.apVerse15}
+              {/* Ale niektorí z nich boli z Cypru a z Kyrény, a keď prišli do
+              Antiochie, hovorili aj Grékom a zvestovali Pána Ježiša.{' '} */}
             </span>
-            <span className='text-[20px]'> Skutky apoštolov 11:20 </span>
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef15} {/* Skutky apoštolov 11:20 */}
+            </span>
             <span className='italic'>
               {' '}
-              Predvolali si ich teda a prikázali im, aby nikdy viac ani
-              nehovorili ani neučili v mene Ježišovom.{' '}
+              {content.apVerse16}
+              {/* Predvolali si ich teda a prikázali im, aby nikdy viac ani
+              nehovorili ani neučili v mene Ježišovom.{' '} */}
             </span>
-            <span className='text-[20px]'> Skutky apoštolov 4:18 </span>
-            Čo teda vadilo ich protivníkom? Meno Ježiš.
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef16} {/* Skutky apoštolov 4:18 */}
+            </span>
+            {content.apText5}
+            {/* Čo teda vadilo ich protivníkom? Meno Ježiš. */}
           </p>
 
           <h2 className='text-center text-[25px] mt-4'>
-            Svedčili o tom, čo hovoril Ježiš Kristus
+            {content.apSub6}
+            {/* Svedčili o tom, čo hovoril Ježiš Kristus */}
           </h2>
 
           <p>
-            Slovo, ktoré hlásal Pán Ježiš, to hlásali apoštoli.
+            {content.apText6}
+            {/* Slovo, ktoré hlásal Pán Ježiš, to hlásali apoštoli. */}
             <span className='italic'>
               {' '}
-              O niekoľko dní povedal Pavel Barnabášovi: Vráťme sa a po všetkých
+              {content.apVerse17}
+              {/* O niekoľko dní povedal Pavel Barnabášovi: Vráťme sa a po všetkých
               mestách, v ktorých sme zvestovali slovo Pánovo, ponavštevujeme
-              bratov, ako sa majú.{' '}
+              bratov, ako sa majú.{' '} */}
             </span>
-            <span className='text-[20px]'> Skutky apoštolov 15:36 </span>
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef17} {/* Skutky apoštolov 15:36 */}
+            </span>
           </p>
 
           <h2 className='text-center text-[25px] mt-4'>
-            Ako apoštoli hlásali evanjelium?
+            {content.apSub7}
+            {/* Ako apoštoli hlásali evanjelium? */}
           </h2>
 
           <p>
             <span className='italic'>
               {' '}
-              Ale prijmite moc Ducha Svätého, ktorý zostúpi na vás, a budete mi
-              svedkami.{' '}
+              {content.apVerse18}
+              {/* Ale prijmite moc Ducha Svätého, ktorý zostúpi na vás, a budete mi
+              svedkami.{' '} */}
             </span>
-            <span className='text-[20px]'> Skutky apoštolov 1:8 </span>
-            Apoštoli nehlásali evanjelium rutinným a rozumovým rozprávaním o
-            tom, čo videli, ale v moci Ducha Svätého.
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef18} {/* Skutky apoštolov 1:8  */}
+            </span>
+            {content.apText7}
+            {/* Apoštoli nehlásali evanjelium rutinným a rozumovým rozprávaním o
+            tom, čo videli, ale v moci Ducha Svätého. */}
           </p>
 
           <h2 className='text-center text-[25px] mt-4'>
-            Čo je dôkazom pravého evanjelia?
+            {content.apSub8}
+            {/* Čo je dôkazom pravého evanjelia? */}
           </h2>
 
           <p>
             <span className='italic'>
               {' '}
-              Veď ja sa nehanbím za evanjelium (Kristovo): lebo mocou Božou je
-              ono na spasenie každému veriacemu, najprv Židovi, a aj Grékovi.{' '}
+              {content.apVerse19}
+              {/* Veď ja sa nehanbím za evanjelium (Kristovo): lebo mocou Božou je
+              ono na spasenie každému veriacemu, najprv Židovi, a aj Grékovi.{' '} */}
             </span>
-            <span className='text-[20px]'> Rimanom 1:16 </span>
-            Spasenie hriešnika je možné len skrze vieru v Ježiša Krista a
-            poslušnosti Jeho Slovu.
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef19} {/* Rimanom 1:16 */}
+            </span>
+            {content.apText8}
+            {/* Spasenie hriešnika je možné len skrze vieru v Ježiša Krista a
+            poslušnosti Jeho Slovu. */}
             <span className='italic'>
               {' '}
-              Zjavuje sa v ňom zaiste spravodlivosť Božia z viery pre vieru, ako
-              je napísané: Spravodlivý z viery bude žiť.{' '}
+              {content.apVerse20}
+              {/* Zjavuje sa v ňom zaiste spravodlivosť Božia z viery pre vieru, ako
+              je napísané: Spravodlivý z viery bude žiť.{' '} */}
             </span>
-            <span className='text-[20px]'> Rimanom 1:17 </span>
-            Vo viere vo vzkriesenie a v nasledovaní Ježiša Krista sa zjavuje
-            Božia spravodlivosť.
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef20} {/* Rimanom 1:17  */}
+            </span>
+            {content.apText9}
+            {/* Vo viere vo vzkriesenie a v nasledovaní Ježiša Krista sa zjavuje
+            Božia spravodlivosť. */}
           </p>
 
           <h2 className='text-center text-[25px] mt-4'>
-            Nehlásať vzkriesenie je porušením Božieho nariadenia
+            {content.apSub9}
+            {/* Nehlásať vzkriesenie je porušením Božieho nariadenia */}
           </h2>
 
           <p>
             <span className='italic'>
               {' '}
-              Veď Boh, ktorému slúžim svojím duchom zvesťou evanjelia Jeho Syna,
-              je mi svedkom, ako neprestajne spomínam na vás.{' '}
+              {content.apVerse21}
+              {/* Veď Boh, ktorému slúžim svojím duchom zvesťou evanjelia Jeho Syna,
+              je mi svedkom, ako neprestajne spomínam na vás.{' '} */}
             </span>
-            <span className='text-[20px]'> Rimanom 1:9 </span>
-            Pavol slúži Bohu tým, že zvestuje evanjelium Jeho Syna. Ak by
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef21} {/* Rimanom 1:9  */}
+            </span>
+            {content.apText10}
+            {/* Pavol slúži Bohu tým, že zvestuje evanjelium Jeho Syna. Ak by
             učeníci nehlásali to, čoho boli očitými svedkami, porušili by príkaz
-            Boha.
+            Boha. */}
             <span className='italic'>
               {' '}
-              Ale Peter a Ján odpovedali im takto: Súďte, či je spravodlivé pred
+              {content.apVerse22}
+              {/* Ale Peter a Ján odpovedali im takto: Súďte, či je spravodlivé pred
               Bohom poslúchať viac vás ako Boha! Lebo my nemôžeme nehovoriť o
-              tom, čo sme videli a počuli.{' '}
+              tom, čo sme videli a počuli.{' '} */}
             </span>
-            <span className='text-[20px]'> Skutky apoštolov 4:19-20 </span>
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef22} {/* Skutky apoštolov 4:19-20 */}
+            </span>
             <span className='italic'>
               {' '}
-              Čudujem sa, že od Toho, ktorý vás povolal v milosti Kristovej, sa
+              {content.apVerse23}
+              {/* Čudujem sa, že od Toho, ktorý vás povolal v milosti Kristovej, sa
               tak rýchlo odvraciate k inému evanjeliu, ktoré ani nie je iné
               evanjelium. Sú však niektorí, čo robia zmätok medzi vami a chcú
               prekrútiť evanjelium Kristovo. Ale keby sme aj my, alebo keby vám
               anjel z neba zvestoval (iné) evanjelium miesto toho, ktoré sme vám
               my zvestovali, - nech je prekliaty! Ako sme prv povedali, aj teraz
               znovu hovorím: ak vám niekto zvestuje (iné) evanjelium miesto
-              toho, ktoré ste prijali, - nech je prekliaty!{' '}
+              toho, ktoré ste prijali, - nech je prekliaty!{' '} */}
             </span>
-            <span className='text-[20px]'> Galatským 1:6-9 </span>
+            <span className='text-[20px]'>
+              {' '}
+              {content.apRef23}
+              {/* Galatským 1:6-9 */}
+            </span>
             <span className='italic'> </span>
             <span className='text-[20px]'></span>
           </p>
